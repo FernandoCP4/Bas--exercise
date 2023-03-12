79102b47462107f59921d3840fba24dba1330d53
= <=>
2023-03-12 10:26:34 -0500

    Pruebitas

---
+++ Band_name.py
@@ -0,0 +1,4 @@
+print("Welcome to the Name Band genertator!")   
+city = input("Which city did you grow up in_\n")
+pet = input("Whats the name of your pet?\n")
+print("The name of the band is:" + city + pet)
---
+++ himom.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Hi Mom"
+sleep 5
+echo "What we gonna eat today?"
+sleep 2
+echo "What a tasty food"
+sleep 4
+echo "Love you mom"
